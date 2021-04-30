d0441214aa06e126ed1883208b47af0bbf60efe5
Deen Liang <deenly.me@gmail.com>
2021-04-30 12:00:46 +0800

    update hello message

--- mod.ts
+++ mod.ts
@@ -1,7 +1,9 @@
+const hello = '<h1>Hello, this is Deen\'s personal website.</h1>'
+
 addEventListener("fetch", (event: FetchEvent) => {
   const ip = event.request.headers.get('x-forwarded-for')
 
-  const response = new Response(`Hello World!\nYour IA address is <b>${ip}</b>`, {
+  const response = new Response(`${hello}<p>Your IP address is <b>${ip}</b></p>`, {
     headers: { "content-type": "text/html" },
   });
   event.respondWith(response);
