0c0dc772692cb9c1d7a0ca4af08d0c4c49371343
Deen Liang <deenly.me@gmail.com>
2021-04-30 11:57:31 +0800

    add get client ip address

--- mod.ts
+++ mod.ts
@@ -1,6 +1,8 @@
-addEventListener("fetch", (event) => {
-  const response = new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
+addEventListener("fetch", (event: FetchEvent) => {
+  const ip = event.request.headers.get('x-forwarded-for')
+
+  const response = new Response(`Hello World!\nYour IA address is <b>${ip}</b>`, {
+    headers: { "content-type": "text/html" },
   });
   event.respondWith(response);
 });
